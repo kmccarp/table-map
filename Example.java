bde73cee36c4d8e20d9b353666b793e64e08e883
Kevin Carpenter (home) <kwcarpenter@gmail.com>
2013-11-12 21:14:04 -0600

    Add more integers

--- Example.java
+++ Example.java
@@ -5,7 +5,7 @@ public class Example {
   public static void main(String[] args) {
     
     
-    Collection<Integer> ints = Arrays.asList(new Integer[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12});
+    Collection<Integer> ints = Arrays.asList(new Integer[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31});
     TableMap<Integer, Integer, Integer> multiplicationTable = new TableMap<Integer, Integer, Integer>(ints, ints, Integer.class);
     for(Integer integ : ints) {
       for (Integer integ2 : ints) {

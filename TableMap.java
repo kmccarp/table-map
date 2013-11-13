ba10051dc515e6e81c64c308cbb739e5f24f7741
Kevin Carpenter (home) <kwcarpenter@gmail.com>
2013-11-12 20:41:02 -0600

    TableMap works

--- TableMap.java
+++ TableMap.java
@@ -1,34 +1,38 @@
 import java.util.Map;
 import java.util.HashMap;
+import java.util.Collection;
+import java.lang.reflect.Array;
 
 public class TableMap<Row, Col, Content> {
   
   private Map<Row, Integer> rows;
   private Map<Col, Integer> cols;
   private Content[][] table;
+  private Class<? extends Content> contentClass;
   
-  public TableMap(Collection<Row> rowCollection, Collection<Col> columnCollection)
+  public TableMap(Collection<Row> rowCollection, Collection<Col> columnCollection, Class<? extends Content> contentClass)
   {
-    rows = new HashMap<>();
+    this.contentClass = contentClass;
+
+    rows = new HashMap<Row, Integer>();
     int c = 0;
     for(Row row : rowCollection) {
       rows.put(row, c++);
     }
     
-    cols = new HashMap<>();
+    cols = new HashMap<Col, Integer>();
     c = 0;
     for (Col col : columnCollection) {
       cols.put(col, c++);
     }
-    
-    table = new Content[rowCollection.size()][colCollection.size()];
+    table = (Content[][]) Array.newInstance(contentClass, rowCollection.size(), columnCollection.size());
   }
   
   public void set(Row row, Col col, Content val) {
     table[rows.get(row)][cols.get(col)] = val;
   }
   
-  public void get(Row row, Col col) {
+  public Content get(Row row, Col col) {
     return table[rows.get(row)][cols.get(col)];
   }
 }
